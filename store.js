64f1bafcbdc43039cc0b1a081c2003434409359e
Anna Mazur <ania.machnik@interia.pl>
2020-03-27 22:39:52 +0000

    Dodano możliwość zmiany statusu zadania

--- store.js
+++ store.js
@@ -1,21 +1,3 @@
-const storage = require('azure-storage')
-const uuid = require('uuid')
-const service = storage.createTableService()
-const table = 'tasks'
-
-
-const init = async () => (
-  new Promise((resolve, reject) => {
-    service.createTableIfNotExists(table, (error, result, response) => {
-      !error ? resolve() : reject()
-    })
-  })
-)
-
-module.exports = {
-  init,
-}
-
 
 const createTask = async (title) => (
   new Promise((resolve, reject) => {
@@ -23,9 +5,9 @@ const createTask = async (title) => (
     const task = {
       PartitionKey: generator.String('task'),
       RowKey: generator.String(uuid.v4()),
-      title
+      title,
+      status: 'open'
     }
-
 
     service.insertEntity(table, task, (error, result, response) => {
       !error ? resolve() : reject()
@@ -38,12 +20,14 @@ const createTask = async (title) => (
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'])
+      .select(['RowKey', 'title', 'status'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
-        title: entry.title._
+        id: entry.RowKey._,
+        title: entry.title._,
+        status: entry.status._
       }))) : reject()
     })
   })
@@ -53,5 +37,6 @@ const listTasks = async () => (
 module.exports = {
   init,
   createTask,
-  listTasks
+  listTasks,
+  updateTaskStatus
 }
